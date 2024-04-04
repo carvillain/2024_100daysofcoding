7cf1eb213668aa3d14a4fed2a8ef33fe8f496615
carvillain <owen.carvill@gmail.com>
2024-04-04 11:15:33 -0400

    Is it a letter?

---
+++ isitletter.py
@@ -0,0 +1,9 @@
+'''
+Code Wars Kata: https://www.codewars.com/kata/57a06b07cf1fa58b2b000252/train/python
+
+Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
+'''
+
+def is_it_letter(s):
+    return s.isalpha()
+
